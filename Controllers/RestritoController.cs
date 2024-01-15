3027f711b4acc92c712d964e70edc83680baa2f3
felipebellina <felipeluizb@hotmail.com>
2024-01-15 19:31:39 -0300

    filtro de usuario adm

---
+++ Controllers/RestritoController.cs
@@ -0,0 +1,14 @@
+﻿using ControleMedicamentos.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleMedicamentos.Controllers
+{
+    [PaginaParaUsuarioLogado]
+    public class RestritoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
